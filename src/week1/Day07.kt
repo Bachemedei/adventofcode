258c2bd29afc0ab05a7a754e208b3e3bdf8fa734
bronwynach-vgw <bronwyn.achemedei@vgw.co>
2021-12-07 20:10:30 +0800

    day 7

--- src/week1/Day07.kt
+++ src/week1/Day07.kt
@@ -1,23 +1,72 @@
 package week1
 
 import readInput
+import kotlin.math.absoluteValue
 
 fun main() {
     fun part1(input: List<String>): Int {
+        val currentPositions = fromStringToInt(input)
+        val desiredPosition = getMedian(currentPositions)
 
-        return 0
+        return currentPositions.fold(0) { totalFuelUsed, currentPosition ->
+            totalFuelUsed + getDistance(currentPosition, desiredPosition)
+        }
     }
 
     fun part2(input: List<String>): Int {
+        val currentPositions = fromStringToInt(input)
 
-        return 0
+        val fuelConsumptionOptOne = currentPositions.fold(0) { totalFuelUsed, currentPosition ->
+                totalFuelUsed + getFuelUsed(currentPosition, getMeanFloor(currentPositions)) }
+
+        val fuelConsumptionOptTwo = currentPositions.fold(0) { totalFuelUsed, currentPosition ->
+            totalFuelUsed + getFuelUsed(currentPosition, getMeanCeiling(currentPositions))
+        }
+
+        return when {
+            fuelConsumptionOptOne < fuelConsumptionOptTwo -> fuelConsumptionOptOne
+            else -> fuelConsumptionOptTwo
+        }
     }
 
-    val testInput = readInput("src/week1", "Day07_test")
-    println("Part 1 test " + if (part1(testInput) == 0) "passed" else "failed")
-    println("Part 2 test " + if (part2(testInput) == 0) "passed" else "failed")
+    fun run() {
+        val testInput = readInput("src/week1", "Day07_test")
+        println("Part 1 test " + if (part1(testInput) == 37) "passed" else "failed")
+        println("Part 2 test " + if (part2(testInput) == 168) "passed" else "failed")
 
-    val input = readInput("src/week1", "Day07")
-    println("Part 1 answer: ${part1(input)}")
-    println("Part 2 answer: ${part2(input)}")
+        val input = readInput("src/week1", "Day07")
+        println("Part 1 answer: ${part1(input)}")
+        println("Part 2 answer: ${part2(input)}")
+    }
+
+    run()
+}
+
+fun fromStringToInt(input: List<String>): List<Int> {
+    return input[0].split(",").map { it.toInt() }.sorted()
+}
+
+fun getMedian(numbers: List<Int>): Int {
+    return numbers[numbers.size.floorDiv(2)]
+}
+
+fun getMeanFloor(numbers: List<Int>): Int {
+    return numbers.sum().floorDiv(numbers.size)
+}
+
+fun getMeanCeiling(numbers: List<Int>): Int {
+    return getMeanFloor(numbers) + 1
+}
+
+fun getDistance(currentPosition: Int, newPosition: Int): Int {
+    return (currentPosition - newPosition).absoluteValue
+}
+
+fun getFuelUsed(currentPosition: Int, newPosition: Int): Int {
+    val distanceToMove = getDistance(currentPosition, newPosition)
+    return calculateArithmeticSum(distanceToMove)
+}
+
+fun calculateArithmeticSum(n: Int): Int {
+    return (n * (n + 1) / 2)
 }
